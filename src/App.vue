4eae470872a1dae176724f1ee8f140e75df52d07
YoimelDev <yoimeldev@gmail.com>
2022-10-22 14:12:21 -0400

    styles: add h1

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 </script>
 
 <template>
-  <h1 class="text-4xl font-black">Hola Mundo</h1>
+  <div class="my-20 p-10 max-w-lg mx-auto bg-white shadow">
+    <h1 class="text-4xl font-extrabold text-gray-500 text-center">
+      Cuanto <span class="text-indigo-600">dinero</span> necesitas?
+    </h1>
+  </div>
 </template>
 
