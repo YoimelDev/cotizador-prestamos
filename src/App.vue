338549f53da599b704d2ba6e8116cf6839fe0d1f
YoimelDev <yoimeldev@gmail.com>
2022-10-22 16:32:42 -0400

    feat: add compute property

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
-import { reactive, ref } from "@vue/reactivity";
+import { ref, computed } from "vue";
 import Header from "./components/Header.vue";
 
 const quantity = ref(10000);
 const MIN = 0;
 const MAX = 20000;
 const STEP = 100;
+
+const formatMoney = computed(() => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  return formatter.format(quantity.value);
+});
 </script>
 
 <template>
@@ -22,7 +31,9 @@ const STEP = 100;
         v-model.number="quantity"
       />
 
-      <p>$ {{ quantity }}</p>
+      <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">
+        {{ formatMoney }}
+      </p>
     </div>
   </div>
 </template>
